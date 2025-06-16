4960de520d84a2f635788df49dff5928f9c644a4
Devilxking789 <panjalphukan9@gmail.com>
2025-06-16 21:48:02 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'SAYAN MD',
+  author: '‎',
+  botName: "SAYAN MD",
+  botOwner: 'SAYAN', // Your name
+  ownerNumber: '918293007159', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
